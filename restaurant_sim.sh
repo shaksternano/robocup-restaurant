0351d07e1de545945361ff755c34a4e92f01e727
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-04-03 19:29:52 +0100

    chore: create launch files for restaurant sim

--- restaurant_sim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/tiago_public_ws
-source devel/setup.bash
-roslaunch tiago_2dnav_gazebo tiago_navigation.launch public_sim:=true world:=restaurant
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ~/catkin_ws/src/robocup-restaurant/restaurant/worlds/restaurant.world ~/tiago_public_ws/src/pal_gazebo_worlds/worlds
-cp -R ~/tiago_public_ws/src/tiago_navigation/tiago_maps/configurations/small_office ~/tiago_public_ws/src/tiago_navigation/tiago_maps/configurations/restaurant
